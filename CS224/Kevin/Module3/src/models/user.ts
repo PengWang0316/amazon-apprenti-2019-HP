0a1b06d552afbcdc2a4ca381948456f69086b859
Kevin Wang <rocwang0316@gmail.com>
2019-08-21 13:52:55 -0700

    Removed the test property

--- CS224/Kevin/Module3/src/models/user.ts
+++ CS224/Kevin/Module3/src/models/user.ts
@@ -5,7 +5,6 @@ export type UserModel = mongoose.Document & {
   password: string;
   email: string;
   age: number;
-  test: object;
 };
 
 // Build a schema and use it to do the validation
@@ -14,14 +13,13 @@ const schema = new mongoose.Schema({
   password: { type: String, required: true },
   email: { type: String, required: true },
   age: { type: Number, required: true, min: 0, max: 200 },
-  test: { type: Map, of: String }
 });
 
 const User = mongoose.model<UserModel>('User', schema);
 
 // Create a new user in the database
 export const createUser = (username: string, password: string, email: string, age: number ) => {
-  new User({ username, password, age, email, test: { 'abc': '223', 'ef': '332' } }).save();
+  new User({ username, password, age, email }).save();
 };
 
 // Find a user based on the username
