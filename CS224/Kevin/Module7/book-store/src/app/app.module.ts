e00230907aa178da8e85b13d81027b61351254e4
Kevin Wang <rocwang0316@gmail.com>
2019-09-10 11:12:33 -0700

    Added more code for list books functionality.

--- CS224/Kevin/Module7/book-store/src/app/app.module.ts
+++ CS224/Kevin/Module7/book-store/src/app/app.module.ts
@@ -10,17 +10,26 @@ import { MatMenuModule } from '@angular/material/menu';
 import { FormsModule } from "@angular/forms";
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookFormComponent } from './book-form/book-form.component';
 import { BirthdayCardFormComponent } from './birthday-card-form/birthday-card-form.component';
+import { BooksComponent } from './books/books.component';
+import { BookDetailDialogComponent } from './book-detail-dialog/book-detail-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookFormComponent,
     BirthdayCardFormComponent,
+    BooksComponent,
+    BookDetailDialogComponent,
+  ],
+  entryComponents: [
+    BookDetailDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,6 +44,8 @@ import { BirthdayCardFormComponent } from './birthday-card-form/birthday-card-fo
     MatSnackBarModule,
     HttpClientModule,
     MatSelectModule,
+    MatDialogModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
