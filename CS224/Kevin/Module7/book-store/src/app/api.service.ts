829bb35ba171fd1b00d01ff524a3c290e1eb9428
Kevin Wang <rocwang0316@gmail.com>
2019-09-11 14:31:46 -0700

    Added the front-end code for edit and delete books

--- CS224/Kevin/Module7/book-store/src/app/api.service.ts
+++ CS224/Kevin/Module7/book-store/src/app/api.service.ts
@@ -9,8 +9,15 @@ export class ApiService {
   private ADD_NEW_BIRTHDAY_CARD_API = 'http://localhost:8080/birthdayCard';
   private FETCH_BOOKS_API = 'http://localhost:8080/books';
   private FETCH_BIRTHDAY_CARDS_API = 'http://localhost:8080/birthdayCards';
+  private FETCH_BOOK_API = 'http://localhost:8080/book';
+  private UPDATE_BOOK_API = this.ADD_NEW_BOOK_API;
+  private DELETE_BOOK_API = this.ADD_NEW_BOOK_API;
 
   constructor(private httpClient: HttpClient) { }
+
+  fetchBook(id: string) {
+    return this.httpClient.get(`${this.FETCH_BOOK_API}/${id}`);
+  }
 
   fetchBooks() {
     return this.httpClient.get(this.FETCH_BOOKS_API);
@@ -20,11 +27,17 @@ export class ApiService {
     return this.httpClient.get(this.FETCH_BIRTHDAY_CARDS_API);
   }
 
-  addNewBook(book: { title: string; isbn: string; author: string; picture: string, price: number }) {
-    return this.httpClient.post(this.ADD_NEW_BOOK_API, book);
+  addOrUpdateBook(
+    book: { title: string; isbn: string; author: string;picture: string, price: number, _id: null | string }) {
+    if (!book._id || book._id === '') return this.httpClient.post(this.ADD_NEW_BOOK_API, book);
+    return this.httpClient.put(this.UPDATE_BOOK_API, book);
   }
 
   addNewBirthCard(birthdayCard: { title: string; material: string; picture: string, price: number }) {
     return this.httpClient.post(this.ADD_NEW_BIRTHDAY_CARD_API, birthdayCard);
+  }
+
+  deleteBook(id: string) {
+    return this.httpClient.delete(`${this.DELETE_BOOK_API}/${id}`);
   }
 }
--- CS224/Kevin/Module7/book-store/src/app/app.module.ts
+++ CS224/Kevin/Module7/book-store/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { FormsModule } from "@angular/forms";
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatSnackBarModule}  from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
