da1dcaaf423509cab8de76fa608aeff31e3a561a
Kevin Wang <rocwang0316@gmail.com>
2019-09-13 15:02:22 -0700

    Updated the API URL

--- CS224/Kevin/Module7/book-store/src/app/api.service.ts
+++ CS224/Kevin/Module7/book-store/src/app/api.service.ts
@@ -5,10 +5,10 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
-  private ADD_NEW_BOOK_API = 'http://localhost:8080/book';
-  private ADD_NEW_BIRTHDAY_CARD_API = 'http://localhost:8080/birthdayCard';
-  private FETCH_BOOKS_API = 'http://localhost:8080/books';
-  private FETCH_BIRTHDAY_CARDS_API = 'http://localhost:8080/birthdayCards';
+  private ADD_NEW_BOOK_API = 'http://ec2-54-149-174-59.us-west-2.compute.amazonaws.com:4000/book';
+  private ADD_NEW_BIRTHDAY_CARD_API = 'http://ec2-54-149-174-59.us-west-2.compute.amazonaws.com:4000/birthdayCard';
+  private FETCH_BOOKS_API = 'http://ec2-54-149-174-59.us-west-2.compute.amazonaws.com:4000/books';
+  private FETCH_BIRTHDAY_CARDS_API = 'http://ec2-54-149-174-59.us-west-2.compute.amazonaws.com:4000/birthdayCards';
   private FETCH_BOOK_API = this.ADD_NEW_BOOK_API;
   private UPDATE_BOOK_API = this.ADD_NEW_BOOK_API;
   private DELETE_BOOK_API = this.ADD_NEW_BOOK_API;
